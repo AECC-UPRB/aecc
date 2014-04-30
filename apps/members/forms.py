fbeaf31e7e9d40442141c364db0046e7542364cc
eluciano11 <emma.luciano11@gmail.com>
2014-04-30 15:38:00 -0400

    deleted unnesary files

--- apps/members/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Member
-
-
-class SignupForm(forms.ModelForm):
-    class Meta:
-        model = Member
-        fields = ['first_name', 'last_name', 'gender', 'email']
--- apps/people/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import People
-
-
-class PeopleForm(forms.ModelForm):
-    class Meta:
-        model = People
-        field = ('name', 'lastname', 'email', 'position')
