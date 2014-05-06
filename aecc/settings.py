1c5971bd33017e415ed2454b32b295293a144a19
Jose E Padilla <jpadilla1293@gmail.com>
2014-05-06 14:20:06 -0400

    added django-dotenv & django-configurations

--- aecc/settings.py
+++ aecc/settings.py
@@ -1,138 +1,145 @@
+from configurations import Configuration, values
+
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-SECRET_KEY = 'jl1gh(r9))nyva)lv@-lty66^k6bn=^y4(iteic#2cv+ud-xnl'
-
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
-# Application definition
+class Common(Configuration):
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
+    ENVIRONMENT = values.Value(environ_prefix=None, default='development')
 
-    # Apps
-    'apps.events',
-    'apps.contact',
-    'apps.users',
-    'apps.blog',
+    SECRET_KEY = values.SecretValue(environ_prefix=None)
 
-    # Third party
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'django_extensions',
-    'debug_toolbar',
-    'import_export',
-    'taggit',
-)
+    DEBUG = False
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    TEMPLATE_DEBUG = True
 
-ROOT_URLCONF = 'aecc.urls'
+    ALLOWED_HOSTS = []
 
-WSGI_APPLICATION = 'aecc.wsgi.application'
+    # Application definition
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+
+        # Apps
+        'apps.events',
+        'apps.contact',
+        'apps.users',
+        'apps.blog',
+
+        # Third party
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'django_extensions',
+        'debug_toolbar',
+        'import_export',
+        'taggit',
+    )
+
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
+
+    ROOT_URLCONF = 'aecc.urls'
+
+    WSGI_APPLICATION = 'aecc.wsgi.application'
+
+    # Database
+    # https://docs.djangoproject.com/en/dev/ref/settings/#databases
+    DATABASES = values.DatabaseURLValue(
+        'sqlite:///{}'.format(os.path.join(BASE_DIR, 'storage.sqlite3')))
+
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
+    LANGUAGE_CODE = 'en-us'
+
+    TIME_ZONE = 'UTC'
+
+    USE_I18N = True
+
+    USE_L10N = True
+
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+    STATIC_URL = '/static/'
+
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        "django.core.context_processors.request",
+        "django.contrib.auth.context_processors.auth",
+        "allauth.account.context_processors.account",
+        "allauth.socialaccount.context_processors.socialaccount",
+    )
+
+    AUTHENTICATION_BACKENDS = (
+        "django.contrib.auth.backends.ModelBackend",
+        "allauth.account.auth_backends.AuthenticationBackend",
+    )
+
+    SITE_ID = 1
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+    TEMPLATE_DIRS = (
+        os.path.join(BASE_DIR, 'templates'),
+    )
+
+    AUTH_USER_MODEL = "users.User"
+
+    # auth and allauth settings
+    LOGIN_REDIRECT_URL = '/'
+    LOGIN_URL = '/accounts/login/'
+    ACCOUNT_EMAIL_VERIFICATION = "none"
+    ACCOUNT_EMAIL_SUBJECT_PREFIX = '[AECC]'
+    ACCOUNT_LOGOUT_ON_GET = True
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+    ACCOUNT_LOGOUT_REDIRECT_URL = LOGIN_URL
+    ACCOUNT_USERNAME_BLACKLIST = ['admin']
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = "email"
+    ACCOUNT_SIGNUP_FORM_CLASS = 'apps.users.forms.SignupForm'
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+    ACCOUNT_USERNAME_REQUIRED = False
+
+    RECAPTCHA_PUBLIC_KEY = '76wtgdfsjhsydt7r5FFGFhgsdfytd656sad75fgh'
+
+    RECAPTCHA_PRIVATE_KEY = '98dfg6df7g56df6gdfgdfg65JHJH656565GFGFGs'
+
+    RECAPTCHA_USE_SSL = True
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+class Development(Common):
+    DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'storage.db'),
+    TEMPLATE_DEBUG = DEBUG
+
+    PROTOCOL = 'http'
+
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    # Dummy cache for development
+    CACHES = {
+        'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
     }
-}
 
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.request",
-    "django.contrib.auth.context_processors.auth",
-    "allauth.account.context_processors.account",
-    "allauth.socialaccount.context_processors.socialaccount",
-)
-
-AUTHENTICATION_BACKENDS = (
-    "django.contrib.auth.backends.ModelBackend",
-    "allauth.account.auth_backends.AuthenticationBackend",
-)
-
-SITE_ID = 1
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
-
-AUTH_USER_MODEL = "users.User"
-
-# auth and allauth settings
-LOGIN_REDIRECT_URL = '/'
-LOGIN_URL = '/accounts/login/'
-ACCOUNT_EMAIL_VERIFICATION = "none"
-ACCOUNT_EMAIL_SUBJECT_PREFIX = '[AECC]'
-ACCOUNT_LOGOUT_ON_GET = True
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_CONFIRM_EMAIL_ON_GET = True
-ACCOUNT_LOGOUT_REDIRECT_URL = LOGIN_URL
-ACCOUNT_USERNAME_BLACKLIST = ['admin']
-ACCOUNT_USER_MODEL_USERNAME_FIELD = "email"
-ACCOUNT_SIGNUP_FORM_CLASS = 'apps.users.forms.SignupForm'
-ACCOUNT_AUTHENTICATION_METHOD = "email"
-ACCOUNT_USERNAME_REQUIRED = False
-
-EMAIL_USE_TLS = True
-
-EMAIL_HOST = "smtp.googlemail.com"
-
-EMAIL_PORT = 587
-
-EMAIL_HOST_USER = "example@example.com"
-
-EMAIL_HOST_PASSWORD = "example"
-
-RECAPTCHA_PUBLIC_KEY = '76wtgdfsjhsydt7r5FFGFhgsdfytd656sad75fgh'
-
-RECAPTCHA_PRIVATE_KEY = '98dfg6df7g56df6gdfgdfg65JHJH656565GFGFGs'
-
-RECAPTCHA_USE_SSL = True
-
-SITE_ID = 1
+class Production(Common):
+    PROTOCOL = 'https'
--- aecc/wsgi.py
+++ aecc/wsgi.py
@@ -4,11 +4,17 @@ WSGI config for aecc project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aecc.settings")
 
-from django.core.wsgi import get_wsgi_application
+
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'DEVELOPMENT').title()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aecc.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', ENVIRONMENT)
+
+from configurations.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
