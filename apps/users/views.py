d5fcd4a9496166a14a481a262ae6c98bb9486630
Emmanuel Luciano <emma.luciano11@gmail.com>
2014-08-15 22:40:15 -0400

    added pagination logic and style

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,7 @@
+import math
+
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import UpdateView, TemplateView, DetailView
+from django.views.generic import UpdateView, ListView, DetailView
 from django.http import Http404
 from django.core.urlresolvers import reverse
 
@@ -9,12 +11,29 @@ from .models import User
 from .forms import SettingsForm
 
 
-class DirectiveView(TemplateView):
+class DirectiveView(ListView):
+    model = User
     template_name = 'users/community.html'
+    paginate_by = 12
 
     def get_context_data(self, **kwargs):
         context = super(DirectiveView, self).get_context_data(**kwargs)
-        context['directive'] = User.objects.filter(amount_payed=15)
+        active_members = User.objects.filter(amount_payed=15)
+        context['directive'] = active_members
+        number_of_pages = math.ceil(
+            float(active_members.count()) / float(self.paginate_by))
+        context['number_of_pages'] = range(int(number_of_pages))
+        page = self.request.GET.get('page')
+        if page is None:
+            context['max_to_present'] = int(self.paginate_by)
+            context['start_with'] = 1
+            context['current_page'] = 1
+        else:
+            max_to_present = int(page) * int(self.paginate_by)
+            start_with = (max_to_present - int(self.paginate_by)) + 1
+            context['max_to_present'] = max_to_present
+            context['start_with'] = start_with
+            context['current_page'] = int(page)
         return context
 
 
